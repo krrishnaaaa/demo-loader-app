ef51e8108c9682052eb737cd9cc883ed164577e4
krrishnaaaa <krishna@pcsalt.com>
2016-07-31 21:25:37 +0530

    added option to select either of the URLs

--- app/src/main/java/com/pcsalt/example/demomphrx/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/demomphrx/MainActivity.java
@@ -1,13 +1,30 @@
 package com.pcsalt.example.demomphrx;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup rgUrls;
+    private static final long UPDATE_AFTER = 60 * 60 * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rgUrls = (RadioGroup) findViewById(R.id.rg_urls);
+    }
+
+    public void loadUrl(View view) {
+        int checkedBtnId = rgUrls.getCheckedRadioButtonId();
+        switch (checkedBtnId) {
+            case R.id.rb_url1:
+                break;
+            case R.id.rb_url2:
+                break;
+        }
     }
 }
