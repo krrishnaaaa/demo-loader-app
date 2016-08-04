07854f95dc4854e28e4000105a153f191c0687af
krrishnaaaa <krishna@pcsalt.com>
2016-08-04 23:19:47 +0530

    added code to load from api if not available in db

--- app/src/main/java/com/pcsalt/example/demomphrx/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/demomphrx/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pcsalt.example.demomphrx;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.pcsalt.example.demomphrx.db.DataSource;
 import com.pcsalt.example.demomphrx.model.WebUrl;
 import com.pcsalt.example.demomphrx.net.NetworkClient;
 import com.pcsalt.example.demomphrx.net.NetworkRepo;
@@ -22,11 +24,26 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup rgUrls;
     private static final long UPDATE_AFTER = 60 * 60 * 1000;
     private static final String TAG = "MainActivity";
+    private DataSource dataSource;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        dataSource.open();
+    }
+
+    @Override
+    protected void onPause() {
+        dataSource.close();
+        super.onPause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataSource = new DataSource(MainActivity.this);
 
         rgUrls = (RadioGroup) findViewById(R.id.rg_urls);
     }
@@ -35,7 +52,11 @@ public class MainActivity extends AppCompatActivity {
         int checkedBtnId = rgUrls.getCheckedRadioButtonId();
         switch (checkedBtnId) {
             case R.id.rb_url1:
-                getFromUrl1();
+                if (dataSource.exists(getString(R.string.url1))) {
+                    openDisplayActivity(getString(R.string.url1));
+                } else {
+                    getFromUrl1();
+                }
                 break;
             case R.id.rb_url2:
                 break;
@@ -51,6 +72,10 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<List<WebUrl>> call, Response<List<WebUrl>> response) {
                 Log.d(TAG, "onResponse: " + response.body().get(0).getWebUrl());
                 Log.d(TAG, "onResponse: " + response.body().get(0).getDescription());
+
+                long insertId = dataSource.insertWebUrl(response.body().get(0), getString(R.string.url1));
+                Log.d(TAG, "insertId: " + insertId);
+                openDisplayActivity(getString(R.string.url1));
             }
 
             @Override
@@ -59,5 +84,11 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void openDisplayActivity(String selectedWebUrl) {
+        Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
+        intent.putExtra("selectedUrl", selectedWebUrl);
+        startActivity(intent);
     }
 }
