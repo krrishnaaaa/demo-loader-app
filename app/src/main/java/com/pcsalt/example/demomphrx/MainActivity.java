b9de8469f04d5847e591f25baf887a96822525c9
krrishnaaaa <krishna@pcsalt.com>
2016-08-04 21:43:01 +0530

    getting information from url

--- app/src/main/java/com/pcsalt/example/demomphrx/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/demomphrx/MainActivity.java
@@ -2,13 +2,26 @@ package com.pcsalt.example.demomphrx;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.pcsalt.example.demomphrx.model.WebUrl;
+import com.pcsalt.example.demomphrx.net.NetworkClient;
+import com.pcsalt.example.demomphrx.net.NetworkRepo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private RadioGroup rgUrls;
     private static final long UPDATE_AFTER = 60 * 60 * 1000;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +35,29 @@ public class MainActivity extends AppCompatActivity {
         int checkedBtnId = rgUrls.getCheckedRadioButtonId();
         switch (checkedBtnId) {
             case R.id.rb_url1:
+                getFromUrl1();
                 break;
             case R.id.rb_url2:
                 break;
         }
+    }
+
+    private void getFromUrl1() {
+        NetworkRepo networkRepo = NetworkClient.getClient().create(NetworkRepo.class);
+
+        Call<List<WebUrl>> call = networkRepo.getFromUrl1();
+        call.enqueue(new Callback<List<WebUrl>>() {
+            @Override
+            public void onResponse(Call<List<WebUrl>> call, Response<List<WebUrl>> response) {
+                Log.d(TAG, "onResponse: " + response.body().get(0).getWebUrl());
+                Log.d(TAG, "onResponse: " + response.body().get(0).getDescription());
+            }
+
+            @Override
+            public void onFailure(Call<List<WebUrl>> call, Throwable t) {
+                Log.e(TAG, "onFailure: ", t);
+                Toast.makeText(MainActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
