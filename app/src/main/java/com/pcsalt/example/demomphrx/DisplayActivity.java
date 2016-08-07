a048ef2358d24b4a91f272f9b68a66a2279cc3ca
krrishnaaaa <krishna@pcsalt.com>
2016-08-07 22:45:14 +0530

    added comments to DisplayActivity.java

--- app/src/main/java/com/pcsalt/example/demomphrx/DisplayActivity.java
+++ app/src/main/java/com/pcsalt/example/demomphrx/DisplayActivity.java
@@ -30,13 +30,21 @@ public class DisplayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
 
+        // getting toolbar view from XML
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // setting the toolbar as ActionBar
         setSupportActionBar(toolbar);
+
+        // getting the TextView inside the Toolbar
         TextView textViewTitle = (TextView) toolbar.findViewById(R.id.text_view_title);
+        // setting Details as the title
         textViewTitle.setText(R.string.title_details);
 
+        // creating DataSource instance to perform database operations
         DataSource dataSource = new DataSource(DisplayActivity.this);
+        // opening database connection
         dataSource.open();
+        // getting WebUrl information from the database
         WebUrl webUrl = dataSource.getWebUrl(getIntent().getStringExtra("selectedUrl"));
         Log.d(TAG, "webUrl: " + webUrl.getWebUrl());
         Log.d(TAG, "description: " + webUrl.getDescription());
@@ -46,6 +54,9 @@ public class DisplayActivity extends AppCompatActivity {
         TextView row2 = (TextView) findViewById(R.id.text_view_row2);
         webView = (WebView) findViewById(R.id.web_view);
 
+        // changing WebSettings for the webView to enable the change of height of WebView as per
+        // the content length and force the URLs to open in the WebView instead of the browser
+        // applications installed in the android device
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
@@ -65,9 +76,16 @@ public class DisplayActivity extends AppCompatActivity {
             @Override
             public void onPageFinished(WebView view, String url) {
                 Log.i(TAG, "Finished loading URL: " + url);
+                // this javaScript method is defined below to change the height of the WebView
+                // after the content is loaded
                 view.loadUrl("javascript:MyApp.resize(document.body.getBoundingClientRect().height)");
             }
 
+            /**
+             * This method will be called after page is finished loading
+             *
+             * @param height
+             */
             @JavascriptInterface
             public void resize(final float height) {
                 DisplayActivity.this.runOnUiThread(new Runnable() {
@@ -85,6 +103,11 @@ public class DisplayActivity extends AppCompatActivity {
         webView.loadUrl(webUrl.getWebUrl());
     }
 
+    /**
+     * This methods checks the length of content in the TextView, if greater than 200 sets to 8sp,
+     * else set it to 14sp.
+     * @param textView TextView instance to resize the text size
+     */
     private void adjustTextSize(@NonNull TextView textView) {
         if (textView.getText().length() > 200) {
             textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 8);
