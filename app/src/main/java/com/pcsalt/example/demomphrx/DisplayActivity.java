5361d31ff1cd8264951cdc1b5b214f3b4d34962a
krrishnaaaa <krishna@pcsalt.com>
2016-08-07 19:03:27 +0530

    setting WebView height to the height of the content

--- app/src/main/java/com/pcsalt/example/demomphrx/DisplayActivity.java
+++ app/src/main/java/com/pcsalt/example/demomphrx/DisplayActivity.java
@@ -5,11 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +19,7 @@ import com.pcsalt.example.demomphrx.model.WebUrl;
 public class DisplayActivity extends AppCompatActivity {
 
     private static final String TAG = "DisplayActivity";
+    WebView webView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,28 +40,38 @@ public class DisplayActivity extends AppCompatActivity {
 
         TextView row1 = (TextView) findViewById(R.id.text_view_row1);
         TextView row2 = (TextView) findViewById(R.id.text_view_row2);
-        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView = (WebView) findViewById(R.id.web_view);
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 Log.i(TAG, "Processing webview url click...");
-                view.loadUrl(String.valueOf(request.getUrl()));
+                view.loadUrl(url);
                 return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(DisplayActivity.this, "Now What: " + description, Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
                 Log.i(TAG, "Finished loading URL: " + url);
+                webView.loadUrl("javascript:MyApp.resize(document.body.getBoundingClientRect().height)");
             }
 
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                Log.e(TAG, "Error: " + error.getDescription());
-                Toast.makeText(DisplayActivity.this, "Now What: " + error.getDescription(), Toast.LENGTH_SHORT).show();
+            @JavascriptInterface
+            public void resize(final float height) {
+                DisplayActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        webView.setLayoutParams(new LinearLayout.LayoutParams(getResources().getDisplayMetrics().widthPixels, (int) (height * getResources().getDisplayMetrics().density)));
+                    }
+                });
             }
         });
 
