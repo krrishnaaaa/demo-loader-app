aa21f57680b5878ce2e9916d758988568ca7b75f
krrishnaaaa <krishna@pcsalt.com>
2016-08-07 21:31:57 +0530

    added comments to NetworkClient.java

--- app/src/main/java/com/pcsalt/example/demomphrx/net/NetworkClient.java
+++ app/src/main/java/com/pcsalt/example/demomphrx/net/NetworkClient.java
@@ -3,11 +3,19 @@ package com.pcsalt.example.demomphrx.net;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * This class is used for getting Retrofit client. This client uses GSON to parse the response
+ * received from the API and place them in proper model objects.
+ */
 public class NetworkClient {
-    public static final String BASE_URL = "http://beta.json-generator.com/api/json/get/";
+    private static final String BASE_URL = "http://beta.json-generator.com/api/json/get/";
     private static Retrofit retrofit = null;
 
-
+    /**
+     * To create the client of the Retrofit
+     *
+     * @return instance of Retrofit
+     */
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
