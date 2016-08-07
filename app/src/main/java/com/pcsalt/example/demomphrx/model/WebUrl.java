5575269759e2b55a83d95257347aeacd6c194368
krrishnaaaa <krishna@pcsalt.com>
2016-08-07 21:29:47 +0530

    added comments to WebUrl.java

--- app/src/main/java/com/pcsalt/example/demomphrx/model/WebUrl.java
+++ app/src/main/java/com/pcsalt/example/demomphrx/model/WebUrl.java
@@ -2,10 +2,20 @@ package com.pcsalt.example.demomphrx.model;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Model call to hold value of webUrl and description, received from the JSON parsing and/or from
+ * the database operations.
+ */
 public class WebUrl {
+    /**
+     * The {@link SerializedName} annotation is used for the key available in the JSON
+     */
     @SerializedName("webUrl")
     private String webUrl;
 
+    /**
+     * The {@link SerializedName} annotation is used for the key available in the JSON
+     */
     @SerializedName("description")
     private String description;
 
