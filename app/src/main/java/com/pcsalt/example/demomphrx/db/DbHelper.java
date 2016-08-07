6a7509dfa87b6403696479c7b38bc3bbd1a6caf5
krrishnaaaa <krishna@pcsalt.com>
2016-08-07 21:26:52 +0530

    added comments to DbHelper.java

--- app/src/main/java/com/pcsalt/example/demomphrx/db/DbHelper.java
+++ app/src/main/java/com/pcsalt/example/demomphrx/db/DbHelper.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+/**
+ * This class creates a table to store the WebUrl information. And provides a reference to the
+ * database to to CRUD operations on the table.
+ */
 class DbHelper extends SQLiteOpenHelper {
 
     static final String TABLE = "url_list";
@@ -19,15 +23,31 @@ class DbHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**
+     * Called when the instance of the database is created. It contains the code to create table(s).
+     * If the database version is upgraded because of change in a table row, then the create
+     * statement should not be changed. Instead, next statement should be written to alter, or add
+     * the column.
+     *
+     * @param sqLiteDatabase instance received from the parent class
+     */
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE + " ( " +
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE + " ( " +
                 ID + " integer primary key autoincrement, " +
                 WEB_URL_ID + " text not null, " +
                 WEB_URL + " text not null, " +
                 DESCRIPTION + " text not null);");
     }
 
+    /**
+     * For now this method does nothing. But, if the database is updated then based on the updation
+     * process, we drop the database, and/or call onCreate().
+     *
+     * @param sqLiteDatabase instance of the database
+     * @param oldVersion     previous version of the database application have
+     * @param newVersion     new version of the database to be created/updated
+     */
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
     }
