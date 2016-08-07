0a3e482df2d99b87e5ceed2064c354e22725a888
krrishnaaaa <krishna@pcsalt.com>
2016-08-07 21:34:47 +0530

    added comments to NetworkRepo.java

--- app/src/main/java/com/pcsalt/example/demomphrx/net/NetworkRepo.java
+++ app/src/main/java/com/pcsalt/example/demomphrx/net/NetworkRepo.java
@@ -7,10 +7,24 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
+/**
+ * This class is the repository class for getting data from API.
+ */
 public interface NetworkRepo {
+
+    /**
+     * This method appends the value with the URL in the Retrofit instance. Creates a GET query.
+     *
+     * @return Call instance
+     */
     @GET("HjRRSIl")
     Call<List<WebUrl>> getFromUrl1();
 
+    /**
+     * This method appends the value with the URL in the Retrofit instance. Creates a GET query.
+     *
+     * @return Call instance
+     */
     @GET("VJ7L0YDVl")
     Call<List<WebUrl>> getFromUrl2();
 }
